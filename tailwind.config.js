44b36117a46d73d495d56fd19a98ad969ea23f5c
Kate Sky <kateskytech@gmail.com>
2021-05-27 11:19:47 -0500

    purge 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [{
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  }],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
