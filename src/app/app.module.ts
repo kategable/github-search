96b152f33deb26977127e059a256c87a7281f6af
Kate Sky <kateskytech@gmail.com>
2021-05-26 19:55:51 -0500

    moving the detiils out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DetailComponent,
 
   ],
   imports: [
